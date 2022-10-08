15d8d04cd897a52eac0bbac0bc6de064f89b9fcd
Satwika Reddy Guntipally <98134060+GuntipallySatwika@users.noreply.github.com>
2022-10-07 20:06:53 -0500

    Tab-bar-view of conference app

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ConferenceScheduler
+//
+//  Created by Guntipally,Satwika Reddy on 10/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
+
