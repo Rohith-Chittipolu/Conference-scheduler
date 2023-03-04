ef1bfd8c50a163bbf76b95dec6fb624ff72fc1a1
Aishwarya Mallela <98127619+AishwaryaMallela@users.noreply.github.com>
2023-03-03 21:06:43 -0600

    Encryption of password in UI and database code.

--- ScreenshotsAndCodeSnippets/FireStoreManager.swift
+++ ScreenshotsAndCodeSnippets/FireStoreManager.swift
@@ -15,6 +15,7 @@ class FireStoreManager {
   
     var dbRefUsers : CollectionReference!
     var dbReConferences : CollectionReference!
+    var dbSpeakers : CollectionReference!
     var db: Firestore!
     let conferenceImagesStorage : StorageReference!
    
@@ -24,6 +25,7 @@ class FireStoreManager {
         db = Firestore.firestore()
         dbRefUsers = db.collection("Users")
         dbReConferences = db.collection("Conferences")
+        dbSpeakers = db.collection("Speakers")
         conferenceImagesStorage = Storage.storage().reference().child("ConferenceImages")
     }
     
@@ -95,6 +97,67 @@ class FireStoreManager {
         }
         
     }
+    /*
+    func getScheduledEventsAsync(conferenceId: String) async -> [EventModel] {
+        let ref = dbReConferences.document(conferenceId).collection("Events")
+        
+        do {
+            let querySnapshot = try await ref.getDocuments()
+
+            var list: [EventModel] = []
+            for event in querySnapshot.documents {
+                let eventRef = event.reference.collection("Subscriber").whereField("email", isEqualTo: UserDefaultsManager.shared.getEmail().lowercased())
+                let eventQuerySnapshot = try await eventRef.getDocuments()
+                
+                if !eventQuerySnapshot.isEmpty {
+                    do {
+                        var data = try event.data(as: EventModel.self)
+                        data.mySubscribeDocumentID = eventQuerySnapshot.documents.first?.documentID ?? "11"
+                        list.append(data)
+                    } catch let error {
+                        print(error)
+                    }
+                }
+            }
+            return list
+        } catch let error {
+            print(error)
+            return []
+        }
+    }
+  */
+    
+    func getScheduledEvents(conferenceId:String,completion: @escaping ([EventModel])->()) {
+        
+        let ref = dbReConferences.document(conferenceId).collection("Events")
+
+           ref.getDocuments { (querySnapshot, err) in
+               if let _ = err {
+                   completion([])
+               } else {
+                   var list: [EventModel] = []
+                   for event in querySnapshot!.documents {
+                       let eventRef = event.reference.collection("Subscriber").whereField("email", isEqualTo: UserDefaultsManager.shared.getEmail().lowercased())
+                       eventRef.getDocuments { (querySnapshot, error) in
+                           if let error = error {
+                               print(error)
+                           } else {
+                               if !querySnapshot!.isEmpty {
+                                   do {
+                                       var data = try event.data(as: EventModel.self)
+                                       data.mySubscribeDocumentID = querySnapshot?.documents.first?.documentID ?? "11"
+                                       list.append(data)
+                                   } catch let error {
+                                       print(error)
+                                   }
+                               }
+                           }
+                           completion(list)
+                       }
+                   }
+               }
+           }
+    }
     
     func checkAlreadyExistAndSignup(userType:UserType,name:String,email:String,password:String) {
         
@@ -115,7 +178,7 @@ class FireStoreManager {
                     
                     // Signup
                     
-                    let data = ["userType" : userType.rawValue,"name":name , "email" : email ,"password" : password]
+                    let data = ["userType" : userType.rawValue,"name":name , "email" : email ,"password" : password.encrypt()]
                     
                     self.addDataToFireStore(data: data) { _ in
                         hideLoading()
@@ -151,6 +214,7 @@ extension FireStoreManager {
     
     func login(email:String,password:String) {
         
+        let password = password.encrypt()
         showLoading()
         
         getQueryFromFirestore(field: "email", compareValue: email) { querySnapshot in
@@ -233,14 +297,14 @@ extension FireStoreManager {
 extension FireStoreManager {
     
  
-    func createEvent(conferenceId:String,eventName:String,eventDescription:String,speaker:String,aboutSpeaker:String,location:String,date:String,time:String,imageUrl:String,completion: @escaping ()->()) {
+    func createEvent(speakerkey:String,conferenceId:String,eventName:String,eventDescription:String,speaker:String,aboutSpeaker:String,location:String,date:String,time:String,imageUrl:String,completion: @escaping ()->()) {
         
         showLoading()
         
         let docRef = dbReConferences.document(conferenceId).collection("Events").document()
         let autoId = docRef.documentID
 
-        let data = ["conferenceId":conferenceId,"key" : autoId, "eventName":eventName, "eventDescription" : eventDescription,"speaker" :speaker ,"aboutSpeaker" : aboutSpeaker , "location" : location ,  "date" :date, "time" : time , "imageUrl" : imageUrl , "speakerkey" : UserDefaultsManager.shared.getKey() ]
+        let data = ["conferenceId":conferenceId,"key" : autoId, "eventName":eventName, "eventDescription" : eventDescription,"speaker" :speaker ,"aboutSpeaker" : aboutSpeaker , "location" : location ,  "date" :date, "time" : time , "imageUrl" : imageUrl , "speakerkey" :  speakerkey]
         
         docRef.setData(data){ err in
             
@@ -257,6 +321,8 @@ extension FireStoreManager {
       }
     }
     
+    
+     
     func saveConference(name:String,url:String,completion: @escaping ()->()) {
         
         showLoading()
@@ -277,6 +343,233 @@ extension FireStoreManager {
                           
                           completion()
                            
+                }
+        }
+      }
+    }
+    
+    func addSpeaker(name:String,details:String,url:String,completion: @escaping ()->()) {
+        
+        showLoading()
+        
+        let docRef = dbSpeakers.document()
+        let autoId = docRef.documentID
+
+        let data = ["key" : autoId, "name":name , "imageUrl" : url , "details" : details]
+        
+        docRef.setData(data) { err in
+            
+            hideLoading()
+            
+                   if let err = err {
+                       showAlertAnyWhere(message: "Error adding document: \(err)")
+                   } else {
+                       showOkAlertAnyWhereWithCallBack(message: "Speaker added!!") {
+                          
+                          completion()
+                           
+                }
+        }
+      }
+    }
+    
+    func scheduleEvent(event:EventModel) {
+        
+        showLoading()
+        
+        let docRef = dbReConferences.document(event.conferenceId).collection("Events").document(event.key).collection("Subscriber")
+      
+        
+        docRef.whereField("email", isEqualTo: UserDefaultsManager.shared.getEmail().lowercased()).getDocuments { (querySnapshot, error) in
+            hideLoading()
+            
+            if let _ = error {
+                showAlertAnyWhere(message: "Error getting documents")
+            } else {
+                if querySnapshot?.isEmpty == false {
+                    showAlertAnyWhere(message: "Event Already Scheduled")
+                    
+                } else {
+                    print("Not subscribed")
+                    self.subscirbe(docRef: docRef)
+                }
+            }
+        }
+    }
+    
+    func subscirbe(docRef:CollectionReference) {
+        
+        let data = ["email" : UserDefaultsManager.shared.getEmail().lowercased() , "token" : UserDefaultsManager.shared.getFirebaseToken()]
+        
+        showLoading()
+        
+        docRef.addDocument(data: data) { err in
+            hideLoading()
+                   if let err = err {
+                       showAlertAnyWhere(message: "Error adding document: \(err)")
+                   } else {
+                       showAlertAnyWhere(message: "Event added to your schedule list")
+            }
+        }
+    }
+    
+    func getSpeakers(completion: @escaping ([SpeakersModel])->()) {
+        
+        self.dbSpeakers.addSnapshotListener { querySnapshot, err in
+             
+            if let _ = err {
+                completion([])
+            }else {
+                
+                var list: [SpeakersModel] = []
+                for document in querySnapshot!.documents {
+                    do {
+                        let data = try document.data(as: SpeakersModel.self)
+                        list.append(data)
+                    }catch let error {
+                        print(error)
+                    }
+                }
+                completion(list)
+            }
+        }
+    }
+    
+    func getSpeaker(key:String,completion: @escaping ([SpeakersModel])->()) {
+        
+        showLoading()
+        
+        let ref = dbSpeakers.document(key)
+        
+        ref.getDocument { snap, error in
+            hideLoading()
+           if let _ = error {
+                completion([])
+            }else {
+                
+                var list: [SpeakersModel] = []
+                 
+                    do {
+                        let data = try snap!.data(as: SpeakersModel.self)
+                        list.append(data)
+                    }catch let error {
+                        print(error)
+                    }
+                
+                completion(list)
+            }
+        }
+    }
+    
+    
+    func updateEvent(event: EventModel, completion: @escaping (Error?) -> Void) {
+      
+        showLoading()
+        do {
+            try dbReConferences.document(event.conferenceId).collection("Events").document(event.key).setData(from: event) { error in
+              hideLoading()
+            if let error = error {
+              completion(error)
+            } else {
+                showOkAlertAnyWhereWithCallBack(message: "Event Updated") {
+                    completion(nil)
+                }
+            }
+          }
+        } catch {
+          completion(error)
+        }
+     }
+   
+    
+  
+    
+    func getSpeakersNoListen(completion: @escaping ([SpeakersModel])->()) {
+        showLoading()
+        self.dbSpeakers.getDocuments { querySnapshot, err in
+         hideLoading()
+            if let _ = err {
+                completion([])
+            }else {
+                
+                var list: [SpeakersModel] = []
+                for document in querySnapshot!.documents {
+                    do {
+                        let data = try document.data(as: SpeakersModel.self)
+                        list.append(data)
+                    }catch let error {
+                        print(error)
+                    }
+                }
+                completion(list)
+            }
+        }
+    }
+    
+    func updateConference(key:String,name:String,url:String,completion: @escaping ()->()) {
+        
+        showLoading()
+        
+        let docRef = dbReConferences.document(key)
+        
+        let data = ["key" : key, "name":name , "imageUrl" : url]
+        
+        docRef.setData(data) { err in
+            
+            hideLoading()
+            
+                   if let err = err {
+                       showAlertAnyWhere(message: "Error adding document: \(err)")
+                   } else {
+                       showOkAlertAnyWhereWithCallBack(message: "Conference Updated!!") {
+                          
+                          completion()
+                           
+                }
+        }
+      }
+    }
+    
+    
+    func deleteConference(key:String,completion: @escaping ()->()) {
+        
+        showLoading()
+        
+        let docRef = dbReConferences.document(key)
+        let eventRef = dbReConferences.document(key).collection("Events")
+        
+        docRef.delete(){ err in
+            
+            hideLoading()
+            
+                   if let err = err {
+                       showAlertAnyWhere(message: "Error deleting document: \(err)")
+                   } else {
+                       showOkAlertAnyWhereWithCallBack(message: "Conference Deleted!!") {
+                          
+                          completion()
+                          eventRef.parent?.delete()
+                           
+                }
+        }
+      }
+    }
+    
+    func removeSchedule(event:EventModel, completion: @escaping ()->()) {
+        
+        showLoading()
+       
+       let docRef = dbReConferences.document(event.conferenceId).collection("Events").document(event.key).collection("Subscriber").document(event.mySubscribeDocumentID!)
+        
+        docRef.delete { err in
+            
+            hideLoading()
+            
+                   if let err = err {
+                       showAlertAnyWhere(message: "Error deleting document: \(err)")
+                   } else {
+                       showOkAlertAnyWhereWithCallBack(message: "Schedule Deleted!!") {
+                          completion()
                 }
         }
       }
