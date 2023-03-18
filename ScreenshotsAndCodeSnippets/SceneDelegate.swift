71c38097fa969425164d2aa96ee3b76f09f254c8
Aishwarya Mallela <98127619+AishwaryaMallela@users.noreply.github.com>
2023-03-17 21:01:51 -0500

    UI Code for Sessions

---
+++ ScreenshotsAndCodeSnippets/SceneDelegate.swift
@@ -0,0 +1,118 @@
+ 
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    static var shared: SceneDelegate?
+    
+    var tempScene : UIScene!
+    
+    var window: UIWindow?
+    
+ 
+    func checkLogin() {
+        
+        
+        guard let windowScene = (tempScene as? UIWindowScene) else { return }
+        
+        SceneDelegate.shared = self
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        var navigationController: UINavigationController!
+    
+        if UserDefaultsManager.shared.isLoggedIn()
+        {
+            
+              let currentTime = Date()
+              let timeElapsed = currentTime.timeIntervalSince(UserDefaults.standard.object(forKey: "lastLoginTime") as! Date )
+              if timeElapsed >  10 * 60 {
+                  
+                        // Auto-logout the user
+                        UserDefaultsManager.shared.clearUserDefaults()
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        let initialViewController = storyboard.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+                        navigationController = UINavigationController(rootViewController: initialViewController)
+                        navigationController.navigationBar.isHidden = false
+                        initialViewController.showSessionExpireAlert()
+                  
+              }else {
+                  
+                  let userType =  UserDefaultsManager.shared.getUserType()
+                  
+                  if(userType == .SUPER_ADMIN) {
+                      
+                      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                      let initialViewController = storyboard.instantiateViewController(withIdentifier: "SuperAdminTabBarHome")
+                      navigationController = UINavigationController(rootViewController: initialViewController)
+                      navigationController.navigationBar.isHidden = false
+                      
+                  }else if(userType == .ADMIN) {
+                      
+                      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                      let initialViewController = storyboard.instantiateViewController(withIdentifier: "AdminTabBarHome")
+                      navigationController = UINavigationController(rootViewController: initialViewController)
+                      navigationController.navigationBar.isHidden = false
+                      
+                  }
+                  else  { // Student
+                      
+                      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                      let initialViewController = storyboard.instantiateViewController(withIdentifier: "StudenTabBarHome")
+                      navigationController = UINavigationController(rootViewController: initialViewController)
+                      navigationController.navigationBar.isHidden = false
+                      
+                  }
+                  
+              }
+                
+                
+        }
+        else
+        {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+            navigationController = UINavigationController(rootViewController: initialViewController)
+            navigationController.navigationBar.isHidden = false
+        
+        
+        }
+        
+        self.window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+       
+        self.tempScene = scene
+        self.checkLogin()
+        
+       
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+    
+    
+}
+
+ 
