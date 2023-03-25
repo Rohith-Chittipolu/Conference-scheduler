8da310b392ddf62d78ed16126d670c515dfbd129
Aishwarya Mallela <98127619+AishwaryaMallela@users.noreply.github.com>
2023-03-24 20:43:59 -0500

    Code for survey link in event details

--- ScreenshotsAndCodeSnippets/EventDetailsVC.swift
+++ ScreenshotsAndCodeSnippets/EventDetailsVC.swift
@@ -5,6 +5,7 @@ class EventDetailsVC: UIViewController {
     
     var event : EventModel!
     
+    @IBOutlet weak var survayLink: UILabel!
     @IBOutlet weak var eventName: UITextField!
     @IBOutlet weak var eventDescription: UITextField!
     @IBOutlet weak var speaker: UITextField!
@@ -14,7 +15,7 @@ class EventDetailsVC: UIViewController {
     @IBOutlet weak var eventImage: UIImageView!
     @IBOutlet weak var speakerNameView: DesignableView!
     @IBOutlet weak var speakerDetailsView: DesignableView!
-    
+    @IBOutlet weak var editButton: UIButton!
     override func viewDidLoad() {
         
         
@@ -23,15 +24,66 @@ class EventDetailsVC: UIViewController {
         self.speakerNameView.addGestureRecognizer(gesture)
         self.speakerDetailsView.addGestureRecognizer(gesture2)
         
+        
+        if( UserDefaultsManager.shared.getUserType() == .STUDENT) {
+            self.editButton.isHidden = true
+        }
+        
     }
     
+    @IBAction func onLink(_ sender: Any) {
+        
+        if(self.survayLink.text!.count > 2) {
+            
+            var newUrl = self.survayLink.text!
+            
+            if(!self.survayLink!.text!.contains("http")) {
+                newUrl = "http://" + self.survayLink!.text!
+            }
+            
+            if let url = URL(string: newUrl),
+               let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue),
+               let match = detector.firstMatch(in:newUrl, options: [], range: NSRange(location: 0, length: newUrl.utf16.count)),
+               match.range == NSRange(location: 0, length: newUrl.utf16.count)
+            {
+                // yourText is a valid URL, so you can open it
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }else {
+                showAlert(message: "Invalid Url")
+            }
+            
+        }
+      
+    }
+    @IBAction func onEdit(_ sender: Any) {
+        
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "EditEventVC") as! EditEventVC
+        vc.event = self.event
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .coverVertical
+        showLoading()
+        self.dismiss(animated: true) {
+            
+            UIApplication.topViewController()!.present(vc, animated: true, completion: nil)
+            hideLoading()
+        }
+    
+    }
     @objc func speakerDetails(sender : UITapGestureRecognizer) {
        
+        FireStoreManager.shared.getSpeaker(key: event.speakerkey) { speakers in
         
-        let speakerDetailsPopupViewController = SpeakerDetailsPopupViewController()
-        speakerDetailsPopupViewController.modalPresentationStyle = .overFullScreen
-        speakerDetailsPopupViewController.modalTransitionStyle = .crossDissolve
-        present(speakerDetailsPopupViewController, animated: true, completion: nil)
+            if(!speakers.isEmpty) {
+                
+                speakerData =  speakers.first
+                let speakerDetailsPopupViewController = SpeakerDetailsPopupViewController()
+                speakerDetailsPopupViewController.modalPresentationStyle = .overFullScreen
+                speakerDetailsPopupViewController.modalTransitionStyle = .crossDissolve
+                self.present(speakerDetailsPopupViewController, animated: true, completion: nil)
+            }
+        }
+        
+      
         
     }
     
@@ -44,18 +96,21 @@ class EventDetailsVC: UIViewController {
         self.aboutSpeaker.text = event.aboutSpeaker
         self.location.text = event.location
         self.dateTime.text = event.date + " " + event.time
-        
+        self.survayLink.text = event.surveyLink
         let eventImageUrl = event.imageUrl
         
-        if(eventImageUrl.count > 5) {
-          
-            let imageURL = URL(string: eventImageUrl)
-            let imageData = try? Data(contentsOf: imageURL!)
-            if let imageData = imageData {
-                let image = UIImage(data: imageData)
-                self.eventImage.image = image
+        if let imageURL = URL(string: eventImageUrl), eventImageUrl.count > 5 {
+            let session = URLSession.shared
+            let task = session.dataTask(with: imageURL) { data, response, error in
+                if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.eventImage.image = image
+                    }
+                }
             }
+            task.resume()
         }
+
         
     }
   
